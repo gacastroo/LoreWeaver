7cd37feb5561957bbbe47e7173505a753d08bbae
Nyx <ga.castro.2019@alumnos.urjc.es>
2025-05-20 04:30:41 +0200

    fix: configuración correcta de CORS para Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,4 @@
 import app from './app.js';
-import cors from 'cors'
 
 
 const PORT = process.env.PORT || 3000;
@@ -8,7 +7,3 @@ app.listen(PORT, () => {
   console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
 });
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}))
