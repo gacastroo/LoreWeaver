6c802de530a044487f1495ca22506e9a564600d9
gcastroo <guillermoandca@gmail.com>
2025-05-20 11:52:22 +0200

    logsTest

--- backend/src/controllers/personaje.controller.js
+++ backend/src/controllers/personaje.controller.js
@@ -307,6 +307,9 @@ export const desasociarHistoriaPersonaje = async (req, res) => {
 };
 
 export const quitarTagPersonaje = async (req, res) => {
+  console.log('PATCH /personajes/:id/quitar-tag recibido');
+  console.log('ID personaje:', req.params.id);
+  console.log('Body recibido:', req.body);
   const userId = getUserIdFromToken(req);
   const { id } = req.params; // id del personaje
   const { tagId } = req.body;
