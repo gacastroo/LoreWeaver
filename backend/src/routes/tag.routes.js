49474a48cbe0a7e1a8d7e97f0ef6c714ef52e52d
gcastroo <guillermoandca@gmail.com>
2025-05-20 15:22:10 +0200

    Ejm

--- backend/src/routes/tag.routes.js
+++ backend/src/routes/tag.routes.js
@@ -10,6 +10,6 @@ import { verifyToken  } from '../middlewares/auth.js'
 const router = express.Router();
 
 router.get('/',verifyToken , obtenerTags);
-router.patch('/:id',verifyToken , eliminarTag);
+router.delete('/:id',verifyToken , eliminarTag);
 router.post('/', verifyToken , crearTag);
 export default router;
